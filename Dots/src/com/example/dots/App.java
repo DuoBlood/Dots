f81c82675ee0cc58309d3bbe2be77f1436865249
DuoBlood <duo.blood@gmail.com>
2014-08-20 17:13:26 +0300

    first commit

---
+++ Dots/src/com/example/dots/App.java
@@ -0,0 +1,16 @@
+package com.example.dots;
+
+import android.app.Application;
+
+public class App extends Application {
+	
+	// Растояние между точками
+	public static int step = 50;
+	
+	// Начальые координаты точек
+	public static int startX = 50;
+	public static int startY = 50;
+	
+	public static int maxRows = 0;
+	public static int maxColumns = 0;
+}
---
+++ Dots/src/com/example/dots/MyCircle.java
@@ -0,0 +1,20 @@
+package com.example.dots;
+
+public class MyCircle {
+	public int x;
+	public int y;
+	
+	public MyCircle(int x, int y) {
+		super();
+		this.x = x;
+		this.y = y;
+	}
+	
+	public int getX() {
+		return x;
+	}
+	
+	public int getY() {
+		return y;
+	}
+}
